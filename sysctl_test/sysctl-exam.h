0512fe534931b3c368963e151f1db41606b9b8cc
wu yawen <sdwuyawen@126.com>
2015-08-17 18:35:44 +0800

    add sysctl

---
+++ sysctl_test/sysctl-exam.h
@@ -0,0 +1,12 @@
+#ifndef _SYSCTL_EXAM_H
+#define _SYSCTL_EXAM_H
+#include <linux/sysctl.h>
+
+#define MY_ROOT (CTL_CPU + 10)
+#define MY_MAX_SIZE 256
+
+enum {
+        MY_INT_EXAM = 1,
+        MY_STRING_EXAM = 2,
+};
+#endif
